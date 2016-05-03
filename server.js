c47d60292dbd059ec41e0ecb6a053490226eaa39
agentv24 <djvariable@gmail.com>
2016-05-03 15:30:49 -0400

    simple routing

--- server.js
+++ server.js
@@ -6,7 +6,18 @@ var app = connect();
 function doFirst(request, response, next){
 
 	console.log("Bacon");
+	next();
 }
+
+function doSecond(request, response, next){
+
+	console.log("Tuna");
+	next();
+}
+
+app.use(doFirst);
+app.use(doSecond);
+
 
 http.createServer(app).listen(8888);
 console.log("Server is running...");
