e98cb4e9ac0230a55126ccc11a54ac58312a5c7f
agentv24 <djvariable@gmail.com>
2016-05-03 15:40:55 -0400

    added routing with connect

--- server.js
+++ server.js
@@ -3,21 +3,26 @@ var http = require('http');
 
 var app = connect();
 
-function doFirst(request, response, next){
+function profile(request, response){
 
-	console.log("Bacon");
-	next();
+	console.log("User requested profile");
+	
 }
 
-function doSecond(request, response, next){
+function forum(request, response){
 
-	console.log("Tuna");
-	next();
+	console.log("User requested forum");
+
+}
+function index(request, response){
+
+	console.log("User requested index");
+
 }
 
-app.use(doFirst);
-app.use(doSecond);
-
+app.use('/profile', profile);
+app.use('/forum', forum);
+app.use('/', index);
 
 http.createServer(app).listen(8888);
 console.log("Server is running...");
